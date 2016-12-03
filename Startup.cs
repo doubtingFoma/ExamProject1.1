e207836f7d12f9583ff8670b2487c81b6a968cf3
Foma <Fomenkof7@gmail.com>
2016-12-03 20:34:59 +0100

    first comit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamProject1._1.Startup))]
+namespace ExamProject1._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
